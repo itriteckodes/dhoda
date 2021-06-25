25b0d09e4227940671b5d36f3c784b35ee05fc40
unknown <itriteckodes@gmail.com>
2021-06-25 13:16:41 +0100

    new from siddique

--- resources/views/user/auth/reset.blade.php
+++ resources/views/user/auth/reset.blade.php
@@ -1,6 +1,6 @@
 @extends('front.layout.index')
 @section('title')
-    <title>User Login  - Ameen Dhoda House</title>
+    <title>User Reset Password  - Ameen Dhoda House</title>
     <meta name="description" content="">
 
     <!--Keywords -->
