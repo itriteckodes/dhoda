b96467dd028c8fa2de3adf36a47585dd8a4fcde3
siddique123453 <siddiqueakbar560@gmail.com>
2021-12-12 22:10:08 +0500

    siddique 12/12

--- resources/views/front/layout/index.blade.php
+++ resources/views/front/layout/index.blade.php
@@ -5,6 +5,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<meta name="facebook-domain-verification" content="vljgzq328nemdqd8574nzbdl3nie5b" />
 	 @yield('title')
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
